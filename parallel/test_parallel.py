b4c8be1acb60ffbd0f9c36c1a835d2141fc9ff3c
Stuart Minchington <stumin@saucelabs.com>
2024-04-24 09:44:47 -0700

    Additional updates

--- parallel/test_parallel.py
+++ parallel/test_parallel.py
@@ -10,8 +10,7 @@ remote_url = "https://ondemand.us-west-1.saucelabs.com:443/wd/hub"
 
 @pytest.fixture(scope="class")
 def driver(request):
-    browser = request.param
-    options = getattr(Options, browser)()  # Get options based on browser type
+    options = Options()
     options.browser_version = 'latest'
     options.platform_name = 'Windows 10'
 
@@ -19,6 +18,7 @@ def driver(request):
     sauce_options['username'] = os.environ.get("SAUCE_USERNAME")
     sauce_options['accessKey'] = os.environ.get("SAUCE_ACCESS_KEY")
     sauce_options['build'] = 'Pytest Parallel Test'
+    sauce_options['name'] = 'Check Page Title'
     options.set_capability('sauce:options', sauce_options)
 
     driver = webdriver.Remote(command_executor=remote_url, options=options)
@@ -26,14 +26,16 @@ def driver(request):
     driver.quit()
 
 
-def test_desktop_version(driver):
-    driver.get("https://www.sony.com/")
-    print(f"Browser version: {driver.capabilities['browserVersion']}")
-    expected_title = 'Sony Group Portal - Home'
-    assert driver.title == expected_title, f"Unexpected title: {driver.title}"
-
-
-@pytest.mark.parametrize("driver", ["chrome", "edge"], indirect=True)
-def test_with_different_browsers(driver):
-    # Run the test with the provided browser through the fixture
-    pass
+@pytest.mark.parametrize(
+    "url, expected_title",
+    [
+        ("https://sony.com", "Sony Group Portal - Home"),
+        ("https://saucelabs.com", "Sauce Labs: Cross Browser Testing, Selenium Testing & Mobile Testing"),
+    ],
+)
+def test_page_title(driver, url, expected_title):
+    print(f"Sauce Session: https://app.saucelabs.com/tests/{driver.session_id}")
+    driver.get(url)
+    status = "passed" if driver.title == expected_title else "failed"
+    driver.execute_script("sauce:job-result={}".format(status))
+    assert driver.title == expected_title, f"Page title mismatch. Expected: {expected_title}, Actual: {driver.title}"
--- pom/views/home_view.py
+++ pom/views/home_view.py
@@ -8,14 +8,18 @@ class HomeView(BaseView):
     USERNAME_FIELD = (AppiumBy.ACCESSIBILITY_ID, 'test-Username')
     PASSWORD_FIELD = (AppiumBy.ACCESSIBILITY_ID, 'test-Password')
     LOGIN_BUTTON = (AppiumBy.ACCESSIBILITY_ID, 'test-LOGIN')
-    SHOPPING_CART = (AppiumBy.XPATH, '//XCUIElementTypeOther[@name="test-Cart"]')
+    ANDROID_SHOPPING_CART = (AppiumBy.XPATH, '//android.view.ViewGroup[@content-desc="test-Cart"]')
+    IOS_SHOPPING_CART = (AppiumBy.XPATH, '//XCUIElementTypeOther[@name="test-Cart"]')
 
     def sign_in(self):
         self.wait_for(self.USERNAME_FIELD).send_keys('standard_user')
         self.wait_for(self.PASSWORD_FIELD).send_keys('secret_sauce')
         self.wait_for(self.LOGIN_BUTTON).click()
-        self.wait_for(self.SHOPPING_CART)
-        status = "passed" if self.SHOPPING_CART else "failed"
+        if BaseView.platformName == 'iOS':
+            shopping_cart = self.wait_for(self.IOS_SHOPPING_CART)
+        elif BaseView.platformName == 'Android':
+            shopping_cart = self.wait_for(self.ANDROID_SHOPPING_CART)
+        else:
+            raise Exception(f"Unsupported platform: {platform}")
+        status = "passed" if shopping_cart else "failed"
         self.driver.execute_script("sauce:job-result={}".format(status))
-
-
