3c2fa1082477ff78c08a57a8107390e35fa77982
Stuart Minchington <stumin@saucelabs.com>
2024-05-20 17:18:08 -0700

    Updating reference to credentials

--- simple_chrome_test.py
+++ simple_chrome_test.py
@@ -2,10 +2,6 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
-username = os.environ.get("SAUCE_USERNAME")
-accessKey = os.environ.get("SAUCE_ACCESS_KEY")
-
-
 def check_version(options):
     driver = webdriver.Remote(command_executor="https://ondemand.us-west-1.saucelabs.com:443/wd/hub", options=options)
     print(f"Sauce Session: https://app.saucelabs.com/tests/"+driver.session_id)
@@ -18,8 +14,8 @@ def test_chrome_version():
     options.browser_version = 'latest'
     options.platform_name = 'Windows 10'
     sauce_options = {}
-    sauce_options['username'] = f'{username}'
-    sauce_options['accessKey'] = f'{accessKey}'
+    sauce_options['username'] = 'sminch'
+    sauce_options['accessKey'] = os.environ.get("SAUCE_ACCESS_KEY")
     sauce_options['build'] = 'Browser Version Check'
     sauce_options['name'] = 'Chrome Latest'
     sauce_options['extendedDebugging'] = True
